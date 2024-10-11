324b93f3a19c074c3a21ecc97cc4ae178029df52
Kyle Harris <kyts925@gmail.com>
2024-10-11 00:09:36 -0400

    test local greeting package

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/ksirrah13/gofun
 
 go 1.23.2
+
+require (
+	github.com/ksirrah13/greetings v0.0.0-00010101000000-000000000000
+	rsc.io/quote v1.5.2
+)
+
+require (
+	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
+	rsc.io/sampler v1.3.0 // indirect
+)
+
+replace github.com/ksirrah13/greetings => ../greetings
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/ksirrah13/greetings"
+	"rsc.io/quote"
+)
 
 func main() {
-	fmt.Println("who is that?")
+	log.SetPrefix("gofun: ")
+	log.SetFlags(0)
+
+	fmt.Printf("who is that? %s\n", quote.Go())
+	greeting, err := greetings.GetGreeting("Kyle")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(greeting)
+
+	greetings.GreetMany("Kyle", "Steph", "Stan")
 }
